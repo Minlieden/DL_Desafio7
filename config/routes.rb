62234c2f24ca89df3d5c51b9f9bb273aa5658869
Minlieden <antonio.munoz.hernan@gmail.com>
2021-10-07 22:36:43 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  post 'post/create', to: 'posts#create', as: 'post_create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+end
